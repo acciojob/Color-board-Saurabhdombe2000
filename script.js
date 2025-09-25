3c544df0463814e0972b9b19ef6431589de133c1
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-09-25 10:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-//your JS code here. If required.
-const container = document.querySelector(".container");
-// const squares = 800;
+ const container = document.querySelector(".container");
+    // const SQUARES = 800;
 
-// Function to generate random color
-function getRandomColor(){
-	const letters = "0123456789ABCDEF";
-	let color = "#";
-	for(let i=0; i<6; i++} {
-	color += letters[math.floor(math.random() * 16)]
-  }
-return color;
-}
+    // Function to generate random color
+    function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
 
- // Create 800 squares
-for (let i=0; i<=800; i++) {
-	const square = document.createElement("div");
-	square.classList.add("square");
-	
+    // Create 800 squares
+    for (let i = 0; i < 800; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
 
-	square.addEventListener("mouseover" () => {
-		const color = getRandomColor;
-		square.style.backgroundColor = color;
+      // On hover, change color
+      square.addEventListener("mouseover", () => {
+        const color = getRandomColor();
+        square.style.backgroundColor = color;
 
-		 // Reset color after 1 second
-		setTimeout(() => {
-			square.style.backgroundColor = "#1d1d1d";
-		}, 1000);
-	});
-	container.appendChild(square);
-}
+        // Reset color after 1 second
+        setTimeout(() => {
+          square.style.backgroundColor = "#1d1d1d";
+        }, 1000);
+      });
 
+      container.appendChild(square);
+    }
